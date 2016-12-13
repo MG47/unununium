2eedad38023385f1725632f84af10db4cc49177f
Mihir Ganu <mihirganu@gmail.com>
2016-12-12 18:57:31 -0800

    Modified getopt for Android. Added print_devicetree, which is the core function

--- dts-utils/dtsinfo.c
+++ dts-utils/dtsinfo.c
@@ -8,18 +8,22 @@
 #include <errno.h>
 #include <string.h>
 #include <unistd.h>
+#include <stdlib.h>
 
 #define SYSFS_CLASS_PATH "/sys/class/"
 #define SYSFS_DEVICE_PATH "/sys/devices/"
+#define SYSFS_DEVICETREE_PATH "/sys/firmware/devicetree/base"
 
 #define OPT_HELP 1
 #define OPT_CLASSES 2
 
 void usage()
 {
-	printf("Help:\n");
 	printf("dtsinfo:\n");
-	printf("Prints information about device tree and associated firmware");
+	printf("Usage: "
+            "./dtsinfo [-h -c]\n"
+            "-h - Usage\n"
+            "-c - Print classes\n"); 
 }
 
 void print_classes()
@@ -43,16 +47,48 @@ void print_classes()
 	closedir(dir);
 }
 
+void print_devicetree()
+{
+	DIR *dir;
+	struct dirent *dirp;
+	int i = 0;
+	char *buf[FILENAME_MAX];
+
+	printf("Device Tree:\n\n");
+
+	dir = opendir(SYSFS_DEVICETREE_PATH);
+	if (!dir) {
+		printf("error: %s\n", strerror(errno));
+		return;
+	}
+
+	while ((dirp = readdir(dir)) != NULL) {
+		if ((strcmp(dirp->d_name, ".")) && (strcmp((dirp->d_name), ".."))) {
+			buf[i] = malloc(FILENAME_MAX * sizeof(char *));
+			sprintf(buf[i], "%s", dirp->d_name);
+			printf("%s\n", buf[i]);
+			i++;
+		}
+	}
+
+	int count = i;
+	for (i = 0; i < count; i++) {
+		if (!(strcmp(buf[i], "model"))) {
+//			printf("Found it");
+				break;
+		}
+	}
+
+	closedir(dir);
+}
+
 int main(int argc, char **argv)
 {
-	char ch;
+    int opt;
 	int flags = 0;
 
-	printf("Device Tree Information:\n");
-
-	while ((ch = getopt(argc, argv, "ch")) != -1) {
-
-		switch (ch) {
+    while ((opt = getopt(argc, argv, "ch")) != -1) {
+        switch (opt) {
 		case 'c':
 			flags |= OPT_CLASSES;
 			break;
@@ -60,14 +96,22 @@ int main(int argc, char **argv)
 		case 'h':
 			flags |= OPT_HELP;
 			break;
+
+		/*
+			TODO 
+			add gpio check flags
+		*/
 		}
 	}
 
 	if (flags & OPT_HELP)
 		usage();
 
-	if (!flags || (flags & OPT_CLASSES))
+	if (flags & OPT_CLASSES)
 		print_classes();
+
+	if (!flags)
+		print_devicetree();
 
 	return 0;
 }
