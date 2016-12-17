6ca0c671e263284948153dff4b0e0fb1e55152dc
Mihir Ganu <mihirganu@gmail.com>
2016-12-16 18:01:01 -0800

    Added initial framework for tiper.c

--- core-utils/tiper.c
+++ core-utils/tiper.c
@@ -10,9 +10,6 @@
 #define REVISION 1
 #define TOOL_NAME "tiper"
 
-#define OPT_HELP 1
-#define OPT_VERSION 2
-
 static void usage()
 {
 	printf("tiper:\n");
@@ -25,13 +22,32 @@ static void usage()
 
 static void print_version()
 {
-	printf("%s version:%d.%d",TOOL_NAME, VERSION, REVISION);
+	printf("%s version:%d.%d\n",TOOL_NAME, VERSION, REVISION);
+}
+
+static FILE *parse_file(char *filename)
+{
+	FILE *stream;
+	/* TODO parse path and filename */
+	stream = fopen(filename, "a+");
+	if (!stream) {
+		printf("error: %s\n", strerror(errno));
+		return NULL;
+	}
+
+	return stream;
+}
+
+static int init_console()
+{
+	return 0;
 }
 
 int tiper_main(int argc, char **argv)
 {
     int opt;
-	int flags = 0;
+	char *file_string;
+	FILE *stream;
 
 	printf("\nTiper Text Editor :\n\n");
 
@@ -43,20 +59,40 @@ int tiper_main(int argc, char **argv)
     while ((opt = getopt(argc, argv, "hv")) != -1) {
         switch (opt) {
 		case 'h':
-			flags |= OPT_HELP;
-			break;
+			usage();
+			return 0;
 		case 'v':
-			flags |= OPT_VERSION;
-			break;
+			print_version();
+			return 0;
 		}
 	}
+		
+	file_string = argv[1];
+	// open dir/parse/create file function, check permissions
 
-	if (flags & OPT_HELP)
-		usage();
+	stream = parse_file(file_string);
+	if (!stream)
+		return 0;
 
-	if (flags & OPT_VERSION)
-		print_version();
+	if (init_console())
+		return 0;
 
-	printf("\n");
+	char *read;
+	read = malloc(sizeof(char *) * 10);
+
+	while (1) {
+	// threaded:
+	// open console window 
+	// read char, write char
+	// update cursor
+	// input and interpret keys
+		// save load exit find 
+	// close resources	 	
+		read[0] = fgetc(stdin);
+		printf("read %s\n", read);
+		fputs(read, stream);break;
+	}
 	return 0;
 }
+
+
